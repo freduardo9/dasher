a03f9c62c81609bab57400e0050ddc0eb6d4c646
freduardo9 <42724928+freduardo9@users.noreply.github.com>
2022-05-22 22:07:13 -0500

    testing velocity with rectangle object

--- dasher.cpp
+++ dasher.cpp
@@ -14,9 +14,21 @@ int main()
     // Create FPS variable 
     const int FPS = 60;
 
+    /*                              Velocity notes: 
+        Velocity in (pixels/frame)
+        - For 60 FPS and 10 velocity: 10 (p/f) x 60 (f/s) = 600 (p/s)
+    */ 
+
     // Initialize Game Window 
     InitWindow(gameWindow.width, gameWindow.height, "Dapper Dasher");
     
+    // Rectangle Structure 
+    const int width{50}; 
+    const int height{80};
+
+    int posY{gameWindow.height - height};
+    int velocity{0};
+
     // Set Target FPS 
     SetTargetFPS(FPS);
 
@@ -26,7 +38,18 @@ int main()
     {
         BeginDrawing();                             // Begin Drawing
         ClearBackground(gameWindow.background);     // Create White background
+
+        if ( IsKeyPressed(KEY_SPACE) )
+        {
+            velocity -= 10;
+        }
+
+        posY += velocity;
+
+
+        DrawRectangle(gameWindow.width/2, posY, width, height, BLUE);
+
         EndDrawing();                               // End Drawing
     }
-    CloseWindow();
+    CloseWindow();      
 }
